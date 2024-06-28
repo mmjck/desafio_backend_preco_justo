9dbff3b3a2cc30dcfb98de6ab0997140e4bf3a75
mmjck <msantosjck@gmail.com>
2024-06-28 18:15:31 -0400

    added format to currency string

--- shop/src/main/java/com/fair_price/shop/domain/useCases/utils/FormatterData.java
+++ shop/src/main/java/com/fair_price/shop/domain/useCases/utils/FormatterData.java
@@ -1,15 +1,29 @@
 package com.fair_price.shop.domain.useCases.utils;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 import com.fair_price.shop.adapters.gateway.database.entities.StatusDuck;
 
 public class FormatterData {
-    public static String formatterHasDiscount(boolean hasDiscount) {
-        return hasDiscount ? "Com desconto": "---";
+    public static String formatHasDiscount(boolean hasDiscount) {
+        return hasDiscount ? "Com desconto" : "---";
     }
 
-    public static String formatterStatus(StatusDuck status){
+    public static String formatStatus(StatusDuck status) {
         return status == StatusDuck.AVAILABLE ? "Disponível" : "Vendido";
     }
 
+    public static String formatPrice(Double value) {
+
+        if(value == null){
+            return "-";
+        }
+        Locale brazilLocale = new Locale("pt", "BR");
+
+        NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance(brazilLocale);
+        return currencyFormatter.format(value);
+
+    }
 
 }
