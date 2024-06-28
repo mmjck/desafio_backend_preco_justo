d8908bdf3d9ab7f5ed059b5730d1ab7da15e3c88
mmjck <msantosjck@gmail.com>
2024-06-28 11:23:48 -0400

    create duck controller

---
+++ shop/src/main/java/com/fair_price/shop/adapters/controllers/DuckController.java
@@ -0,0 +1,31 @@
+package com.fair_price.shop.adapters.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fair_price.shop.adapters.controllers.model.duck.DuckRequest;
+import com.fair_price.shop.domain.useCases.duck.CreateDuckUseCase;
+
+
+@RestController
+public class DuckController {
+     private final CreateDuckUseCase useCase;
+
+    public DuckController(CreateDuckUseCase useCase) {
+        this.useCase = useCase;
+    }
+
+    @PostMapping(value = "/duck")
+    public ResponseEntity<Object> create(@RequestBody @Validated DuckRequest entity) {
+        try {
+            var response = useCase.call(entity);
+            return ResponseEntity.ok().body(response);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+}
