2a6408135421c29201e4c3ef81ee66b1508a1131
mmjck <msantosjck@gmail.com>
2024-06-28 11:28:05 -0400

    create discount entity

---
+++ shop/src/main/java/com/fair_price/shop/adapters/gateway/database/entities/DiscountEntity.java
@@ -0,0 +1,35 @@
+
+package com.fair_price.shop.adapters.gateway.database.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "discounts")
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class DiscountEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id", referencedColumnName = "id")
+    private CustomerEntity customerEntity;
+
+}
