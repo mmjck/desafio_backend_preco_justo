251d6d79fff00df6d7651efdb62c2a04d34cc78a
mmjck <msantosjck@gmail.com>
2024-06-28 11:25:29 -0400

    create model for orders request

---
+++ shop/src/main/java/com/fair_price/shop/adapters/controllers/model/orders/OrdersRequest.java
@@ -0,0 +1,21 @@
+package com.fair_price.shop.adapters.controllers.model.orders;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Builder
+@Getter
+@Setter
+public class OrdersRequest {
+    @JsonProperty(value = "customer_id")
+    private Integer customerId;
+    
+    @JsonProperty(value = "duck_id")
+    private Integer duckId;
+
+
+}
