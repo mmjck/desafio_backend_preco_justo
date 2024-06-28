9dd5b2d0d017da220e77ce2b14c294bcf507c350
mmjck <msantosjck@gmail.com>
2024-06-28 11:25:56 -0400

    create discounts gateway

---
+++ shop/src/main/java/com/fair_price/shop/adapters/gateway/database/DiscountsGateway.java
@@ -0,0 +1,8 @@
+package com.fair_price.shop.adapters.gateway.database;
+
+import com.fair_price.shop.adapters.gateway.database.entities.DiscountEntity;
+
+
+public interface DiscountsGateway {
+    public DiscountEntity create(DiscountEntity entity);
+}
